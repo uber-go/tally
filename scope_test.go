13a01a2a0dd2ade06c307ccc0ca4e83196c88422
John Allen <jra3@users.noreply.github.com>
2016-08-26 16:37:34 -0400

    Better unit tests (#3)
    * test Tagged
    
    * chasing coverage

--- scope_test.go
+++ scope_test.go
@@ -28,26 +28,45 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+type testValue struct {
+	val  int64
+	tags map[string]string
+}
+
 type testStatsReporter struct {
-	sync.RWMutex
+	cg sync.WaitGroup
+	gg sync.WaitGroup
+	tg sync.WaitGroup
 
 	scope Scope
 
-	counters map[string]int64
-	gauges   map[string]int64
-	timers   map[string]time.Duration
+	counters map[string]*testValue
+	gauges   map[string]*testValue
+	timers   map[string]*testValue
 }
 
 func (r *testStatsReporter) ReportCounter(name string, tags map[string]string, value int64) {
-	r.counters[name] = value
+	r.counters[name] = &testValue{
+		val:  value,
+		tags: tags,
+	}
+	r.cg.Done()
 }
 
 func (r *testStatsReporter) ReportGauge(name string, tags map[string]string, value int64) {
-	r.gauges[name] = value
+	r.gauges[name] = &testValue{
+		val:  value,
+		tags: tags,
+	}
+	r.gg.Done()
 }
 
 func (r *testStatsReporter) ReportTimer(name string, tags map[string]string, interval time.Duration) {
-	r.timers[name] = interval
+	r.timers[name] = &testValue{
+		val:  int64(interval),
+		tags: tags,
+	}
+	r.tg.Done()
 }
 
 func (r *testStatsReporter) Flush() {}
@@ -55,69 +74,167 @@ func (r *testStatsReporter) Flush() {}
 // newTestStatsReporter returns a new TestStatsReporter
 func newTestStatsReporter() *testStatsReporter {
 	return &testStatsReporter{
-		counters: make(map[string]int64),
-		gauges:   make(map[string]int64),
-		timers:   make(map[string]time.Duration),
+		counters: make(map[string]*testValue),
+		gauges:   make(map[string]*testValue),
+		timers:   make(map[string]*testValue),
 	}
+}
+
+func TestWriteTimerImmediately(t *testing.T) {
+	r := newTestStatsReporter()
+	scope := NewRootScope("", nil, r, 0)
+	r.tg.Add(1)
+	scope.Timer("ticky").Record(time.Millisecond * 175)
+	r.tg.Wait()
+}
+
+func TestWriteTimerClosureImmediately(t *testing.T) {
+	r := newTestStatsReporter()
+	scope := NewRootScope("", nil, r, 0)
+	r.tg.Add(1)
+	scope.Timer("ticky").Begin()()
+	r.tg.Wait()
+}
+
+func TestWriteReportLoop(t *testing.T) {
+	r := newTestStatsReporter()
+	scope := NewRootScope("", nil, r, 10)
+	defer scope.Close()
+
+	r.cg.Add(1)
+	scope.Counter("bar").Inc(1)
+	r.gg.Add(1)
+	scope.Gauge("zed").Update(1)
+	r.tg.Add(1)
+	scope.Timer("ticky").Record(time.Millisecond * 175)
+
+	r.cg.Wait()
+	r.gg.Wait()
+	r.tg.Wait()
 }
 
 func TestWriteOnce(t *testing.T) {
 	r := newTestStatsReporter()
+
 	scope := NewRootScope("", nil, r, 0)
+
+	r.cg.Add(1)
 	scope.Counter("bar").Inc(1)
+	r.gg.Add(1)
 	scope.Gauge("zed").Update(1)
+	r.tg.Add(1)
 	scope.Timer("ticky").Record(time.Millisecond * 175)
 
 	scope.Report(r)
-	assert.EqualValues(t, 1, r.counters["bar"])
-	assert.EqualValues(t, 1, r.gauges["zed"])
-	assert.EqualValues(t, time.Millisecond*175, r.timers["ticky"])
+	r.cg.Wait()
+	r.gg.Wait()
+	r.tg.Wait()
+
+	assert.EqualValues(t, 1, r.counters["bar"].val)
+	assert.EqualValues(t, 1, r.gauges["zed"].val)
+	assert.EqualValues(t, time.Millisecond*175, r.timers["ticky"].val)
 
 	r = newTestStatsReporter()
 	scope.Report(r)
-	assert.EqualValues(t, 0, r.counters["bar"])
-	assert.EqualValues(t, 0, r.gauges["zed"])
-	assert.EqualValues(t, 0, r.timers["ticky"])
+
+	assert.Nil(t, r.counters["bar"])
+	assert.Nil(t, r.gauges["zed"])
+	assert.Nil(t, r.timers["ticky"])
 }
 
 func TestRootScopeWithoutPrefix(t *testing.T) {
 	r := newTestStatsReporter()
+
 	scope := NewRootScope("", nil, r, 0)
+	r.cg.Add(1)
 	scope.Counter("bar").Inc(1)
 	scope.Counter("bar").Inc(20)
+	r.gg.Add(1)
 	scope.Gauge("zed").Update(1)
+	r.tg.Add(1)
 	scope.Timer("blork").Record(time.Millisecond * 175)
 
 	scope.Report(r)
-	assert.EqualValues(t, 21, r.counters["bar"])
-	assert.EqualValues(t, 1, r.gauges["zed"])
-	assert.EqualValues(t, time.Millisecond*175, r.timers["blork"])
+	r.cg.Wait()
+	r.gg.Wait()
+	r.tg.Wait()
+
+	assert.EqualValues(t, 21, r.counters["bar"].val)
+	assert.EqualValues(t, 1, r.gauges["zed"].val)
+	assert.EqualValues(t, time.Millisecond*175, r.timers["blork"].val)
 }
 
 func TestRootScopeWithPrefix(t *testing.T) {
 	r := newTestStatsReporter()
+
 	scope := NewRootScope("foo", nil, r, 0)
+	r.cg.Add(1)
 	scope.Counter("bar").Inc(1)
 	scope.Counter("bar").Inc(20)
+	r.gg.Add(1)
 	scope.Gauge("zed").Update(1)
+	r.tg.Add(1)
 	scope.Timer("blork").Record(time.Millisecond * 175)
 
 	scope.Report(r)
-	assert.EqualValues(t, 21, r.counters["foo.bar"])
-	assert.EqualValues(t, 1, r.gauges["foo.zed"])
-	assert.EqualValues(t, time.Millisecond*175, r.timers["foo.blork"])
+	r.cg.Wait()
+	r.gg.Wait()
+	r.tg.Wait()
+
+	assert.EqualValues(t, 21, r.counters["foo.bar"].val)
+	assert.EqualValues(t, 1, r.gauges["foo.zed"].val)
+	assert.EqualValues(t, time.Millisecond*175, r.timers["foo.blork"].val)
 }
 
 func TestSubScope(t *testing.T) {
 	r := newTestStatsReporter()
+
 	scope := NewRootScope("foo", nil, r, 0).SubScope("mork")
+	r.cg.Add(1)
 	scope.Counter("bar").Inc(1)
 	scope.Counter("bar").Inc(20)
+	r.gg.Add(1)
 	scope.Gauge("zed").Update(1)
+	r.tg.Add(1)
 	scope.Timer("blork").Record(time.Millisecond * 175)
 
 	scope.Report(r)
-	assert.EqualValues(t, 21, r.counters["foo.mork.bar"])
-	assert.EqualValues(t, 1, r.gauges["foo.mork.zed"])
-	assert.EqualValues(t, time.Millisecond*175, r.timers["foo.mork.blork"])
+	r.cg.Wait()
+	r.gg.Wait()
+	r.tg.Wait()
+
+	assert.EqualValues(t, 21, r.counters["foo.mork.bar"].val)
+	assert.EqualValues(t, 1, r.gauges["foo.mork.zed"].val)
+	assert.EqualValues(t, time.Millisecond*175, r.timers["foo.mork.blork"].val)
+}
+
+func TestTaggedSubScope(t *testing.T) {
+	r := newTestStatsReporter()
+
+	ts := map[string]string{"env": "test"}
+	scope := NewRootScope("foo", ts, r, 0)
+
+	tscope := scope.Tagged(map[string]string{"service": "test"})
+
+	r.cg.Add(1)
+	scope.Counter("beep").Inc(1)
+	r.cg.Add(1)
+	tscope.Counter("boop").Inc(1)
+
+	scope.Report(r)
+	tscope.Report(r)
+	r.cg.Wait()
+
+	assert.EqualValues(t, 1, r.counters["foo.beep"].val)
+	assert.EqualValues(t, ts, r.counters["foo.beep"].tags)
+
+	assert.EqualValues(t, 1, r.counters["foo.boop"].val)
+	assert.EqualValues(t, map[string]string{
+		"env":     "test",
+		"service": "test",
+	}, r.counters["foo.boop"].tags)
+}
+
+func TestNilTagMerge(t *testing.T) {
+	assert.Nil(t, nil, mergeRightTags(nil, nil))
 }
