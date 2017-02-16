e3760448640448c39d9c0340b61b4544a16f52cf
Rob Skillington <r@uber.com>
2017-02-16 14:43:44 -0500

    Add further test coverage

--- histogram_test.go
+++ histogram_test.go
@@ -29,6 +29,18 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
+func TestValueBucketsString(t *testing.T) {
+	result, err := LinearValueBuckets(1, 1, 3)
+	require.NoError(t, err)
+	assert.Equal(t, "[1.000000 2.000000 3.000000]", Buckets(result).String())
+}
+
+func TestDurationBucketsString(t *testing.T) {
+	result, err := LinearDurationBuckets(time.Second, time.Second, 3)
+	require.NoError(t, err)
+	assert.Equal(t, "[1s 2s 3s]", Buckets(result).String())
+}
+
 func TestBucketPairsDefaultsToNegInfinityToInfinity(t *testing.T) {
 	pairs := BucketPairs(nil)
 	require.Equal(t, 1, len(pairs))
@@ -38,6 +50,40 @@ func TestBucketPairsDefaultsToNegInfinityToInfinity(t *testing.T) {
 
 	assert.Equal(t, time.Duration(math.MinInt64), pairs[0].LowerBoundDuration())
 	assert.Equal(t, time.Duration(math.MaxInt64), pairs[0].UpperBoundDuration())
+}
+
+func TestBucketPairsSortsValueBuckets(t *testing.T) {
+	pairs := BucketPairs(ValueBuckets{1.0, 3.0, 2.0})
+	require.Equal(t, 4, len(pairs))
+
+	assert.Equal(t, -math.MaxFloat64, pairs[0].LowerBoundValue())
+	assert.Equal(t, 1.0, pairs[0].UpperBoundValue())
+
+	assert.Equal(t, 1.0, pairs[1].LowerBoundValue())
+	assert.Equal(t, 2.0, pairs[1].UpperBoundValue())
+
+	assert.Equal(t, 2.0, pairs[2].LowerBoundValue())
+	assert.Equal(t, 3.0, pairs[2].UpperBoundValue())
+
+	assert.Equal(t, 3.0, pairs[3].LowerBoundValue())
+	assert.Equal(t, math.MaxFloat64, pairs[3].UpperBoundValue())
+}
+
+func TestBucketPairsSortsDurationBuckets(t *testing.T) {
+	pairs := BucketPairs(DurationBuckets{0 * time.Second, 2 * time.Second, 1 * time.Second})
+	require.Equal(t, 4, len(pairs))
+
+	assert.Equal(t, time.Duration(math.MinInt64), pairs[0].LowerBoundDuration())
+	assert.Equal(t, 0*time.Second, pairs[0].UpperBoundDuration())
+
+	assert.Equal(t, 0*time.Second, pairs[1].LowerBoundDuration())
+	assert.Equal(t, 1*time.Second, pairs[1].UpperBoundDuration())
+
+	assert.Equal(t, 1*time.Second, pairs[2].LowerBoundDuration())
+	assert.Equal(t, 2*time.Second, pairs[2].UpperBoundDuration())
+
+	assert.Equal(t, 2*time.Second, pairs[3].LowerBoundDuration())
+	assert.Equal(t, time.Duration(math.MaxInt64), pairs[3].UpperBoundDuration())
 }
 
 func TestBucketPairsDurationBucketsInsertsMissingZero(t *testing.T) {
@@ -59,8 +105,82 @@ func TestBucketPairsDurationBucketsInsertsMissingZero(t *testing.T) {
 	assert.Equal(t, time.Duration(math.MaxInt64), pairs[len(pairs)-1].UpperBoundDuration())
 }
 
-func TestLinearValueBuckets(t *testing.T) {
-	result, err := LinearValueBuckets(1, 1, 3)
-	require.NoError(t, err)
-	assert.Equal(t, "[1.000000 2.000000 3.000000]", Buckets(result).String())
+func TestMustMakeLinearValueBuckets(t *testing.T) {
+	assert.NotPanics(t, func() {
+		assert.Equal(t, ValueBuckets{
+			0.0, 1.0, 2.0,
+		}, MustMakeLinearValueBuckets(0, 1, 3))
+	})
+}
+
+func TestMustMakeLinearValueBucketsPanicsOnBadCount(t *testing.T) {
+	assert.Panics(t, func() {
+		MustMakeLinearValueBuckets(0, 1, 0)
+	})
+}
+
+func TestMustMakeLinearDurationBuckets(t *testing.T) {
+	assert.NotPanics(t, func() {
+		assert.Equal(t, DurationBuckets{
+			0, time.Second, 2 * time.Second,
+		}, MustMakeLinearDurationBuckets(0*time.Second, 1*time.Second, 3))
+	})
+}
+
+func TestMustMakeLinearDurationBucketsPanicsOnBadCount(t *testing.T) {
+	assert.Panics(t, func() {
+		MustMakeLinearDurationBuckets(0*time.Second, 1*time.Second, 0)
+	})
+}
+
+func TestMustMakeExponentialValueBuckets(t *testing.T) {
+	assert.NotPanics(t, func() {
+		assert.Equal(t, ValueBuckets{
+			2, 4, 8,
+		}, MustMakeExponentialValueBuckets(2, 2, 3))
+	})
+}
+
+func TestMustMakeExponentialValueBucketsPanicsOnBadCount(t *testing.T) {
+	assert.Panics(t, func() {
+		MustMakeExponentialValueBuckets(2, 2, 0)
+	})
+}
+
+func TestMustMakeExponentialValueBucketsPanicsOnBadStart(t *testing.T) {
+	assert.Panics(t, func() {
+		MustMakeExponentialValueBuckets(0, 2, 2)
+	})
+}
+
+func TestMustMakeExponentialValueBucketsPanicsOnBadFactor(t *testing.T) {
+	assert.Panics(t, func() {
+		MustMakeExponentialValueBuckets(2, 1, 2)
+	})
+}
+
+func TestMustMakeExponentialDurationBuckets(t *testing.T) {
+	assert.NotPanics(t, func() {
+		assert.Equal(t, DurationBuckets{
+			2 * time.Second, 4 * time.Second, 8 * time.Second,
+		}, MustMakeExponentialDurationBuckets(2*time.Second, 2, 3))
+	})
+}
+
+func TestMustMakeExponentialDurationBucketsPanicsOnBadCount(t *testing.T) {
+	assert.Panics(t, func() {
+		MustMakeExponentialDurationBuckets(2*time.Second, 2, 0)
+	})
+}
+
+func TestMustMakeExponentialDurationBucketsPanicsOnBadStart(t *testing.T) {
+	assert.Panics(t, func() {
+		MustMakeExponentialDurationBuckets(0, 2, 2)
+	})
+}
+
+func TestMustMakeExponentialDurationBucketsPanicsOnBadFactor(t *testing.T) {
+	assert.Panics(t, func() {
+		MustMakeExponentialDurationBuckets(2*time.Second, 1, 2)
+	})
 }
