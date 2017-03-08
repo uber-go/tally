be8bb148a46c89fb1db5782589cfd110f00dd6a3
Jerome Froelich <jeromefroe@uber.com>
2017-03-08 14:28:09 -0500

    Adjust size of buffer

--- scope.go
+++ scope.go
@@ -504,7 +504,7 @@ func (s *scope) uniqueID(name string, tags map[string]string) string {
 	}
 
 	// Size of the buffer includes separator bytes
-	buf := bytes.NewBuffer(make([]byte, 0, l+2*len(tags)+1))
+	buf := bytes.NewBuffer(make([]byte, 0, l+2*len(tags)))
 	buf.WriteString(name)
 	buf.WriteString(componentSeparator)
 
