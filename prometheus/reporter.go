dba9fd6e3b71595f6708193e34a994bc9845d1fe
Vytenis Darulis <vytenis@uber.com>
2022-06-03 11:02:59 -0400

    Update vendored Prometheus client and related dependencies (#176)

--- prometheus/reporter.go
+++ prometheus/reporter.go
@@ -163,8 +163,8 @@ type cachedMetric struct {
 	counter     prom.Counter
 	gauge       prom.Gauge
 	reportTimer func(d time.Duration)
-	histogram   prom.Histogram
-	summary     prom.Summary
+	histogram   prom.Observer
+	summary     prom.Observer
 }
 
 func (m *cachedMetric) ReportCount(value int64) {
