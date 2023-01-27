9ad201e1d9a37d4cb01fc02afdcd0a312d096486
brawndou <112038567+brawndou@users.noreply.github.com>
2023-01-26 16:59:03 -0800

    Revert "fix broken internal metrics names and make them optional (#203)"
    This reverts commit 24b844bfdc627df021d0fee64b2d17a982224cc7.

--- scope.go
+++ scope.go
@@ -1,4 +1,4 @@
-// Copyright (c) 2023 Uber Technologies, Inc.
+// Copyright (c) 2022 Uber Technologies, Inc.
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
@@ -28,13 +28,7 @@ import (
 	"go.uber.org/atomic"
 )
 
-type InternalMetricOption int
-
 const (
-	Unset InternalMetricOption = iota
-	SendInternalMetrics
-	OmitInternalMetrics
-
 	_defaultInitialSliceSize = 16
 )
 
@@ -101,15 +95,15 @@ type scope struct {
 
 // ScopeOptions is a set of options to construct a scope.
 type ScopeOptions struct {
-	Tags                  map[string]string
-	Prefix                string
-	Reporter              StatsReporter
-	CachedReporter        CachedStatsReporter
-	Separator             string
-	DefaultBuckets        Buckets
-	SanitizeOptions       *SanitizeOptions
-	registryShardCount    uint
-	internalMetricsOption InternalMetricOption
+	Tags                map[string]string
+	Prefix              string
+	Reporter            StatsReporter
+	CachedReporter      CachedStatsReporter
+	Separator           string
+	DefaultBuckets      Buckets
+	SanitizeOptions     *SanitizeOptions
+	registryShardCount  uint
+	skipInternalMetrics bool
 }
 
 // NewRootScope creates a new root Scope with a set of options and
@@ -179,7 +173,7 @@ func newRootScope(opts ScopeOptions, interval time.Duration) *scope {
 	s.tags = s.copyAndSanitizeMap(opts.Tags)
 
 	// Register the root scope
-	s.registry = newScopeRegistryWithShardCount(s, opts.registryShardCount, opts.internalMetricsOption)
+	s.registry = newScopeRegistryWithShardCount(s, opts.registryShardCount, opts.skipInternalMetrics)
 
 	if interval > 0 {
 		s.wg.Add(1)
