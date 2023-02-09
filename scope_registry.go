b88544fe8225724c23c7fe8e6f28a06e1cc92920
brawndou <112038567+brawndou@users.noreply.github.com>
2023-02-09 10:41:00 -0800

    Update scope_registry.go
    remove log.printf

--- scope_registry.go
+++ scope_registry.go
@@ -262,7 +262,6 @@ func (r *scopeRegistry) reportInternalMetrics() {
 	counters.Add(rootCounters.Load())
 	gauges.Add(rootGauges.Load())
 	histograms.Add(rootHistograms.Load())
-	log.Printf("counters: %v, gauges: %v, histograms: %v\n", counters.Load(), gauges.Load(), histograms.Load())
 
 	if r.root.reporter != nil {
 		r.root.reporter.ReportCounter(r.sanitizedCounterCardinalityName, internalTags, counters.Load())
