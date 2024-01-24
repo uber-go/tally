c74035ac4bb99ea0fc7b25e1ce2bdd52a777c4a2
linhai zhu <brawndo@uber.com>
2024-01-24 13:48:14 -0800

    move comment

--- generate.go
+++ generate.go
@@ -1,7 +1,6 @@
 package tally
 
 import (
+	//go:generate mockgen -package tallymock -destination tallymock/stats_reporter.go -imports github.com/uber-go/tally github.com/uber-go/tally StatsReporter
 	_ "github.com/golang/mock/mockgen/model"
 )
-
-//go:generate mockgen -package tallymock -destination tallymock/stats_reporter.go -imports github.com/uber-go/tally github.com/uber-go/tally StatsReporter
