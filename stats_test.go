e2a71ea0ab75eb085f65b326434cd2c1b8748955
Matas Gaulia <39590298+MGaulia@users.noreply.github.com>
2024-03-21 17:01:17 +0100

    add possibility to record histogram with weight (#252)
    * add possibility to record histogram with weight
    
    * add the same for recording duration

--- stats_test.go
+++ stats_test.go
@@ -139,11 +139,14 @@ func TestHistogramValueSamples(t *testing.T) {
 	for i := 0; i < 5; i++ {
 		h.RecordValue(offset + rand.Float64()*10)
 	}
+	offset = 60
+	h.RecordValueWithWeight(offset+rand.Float64()*10, 2)
 
 	h.report(h.name, h.tags, r)
 
 	assert.Equal(t, 3, r.valueSamples[10.0])
 	assert.Equal(t, 5, r.valueSamples[60.0])
+	assert.Equal(t, 2, r.valueSamples[70.0])
 	assert.Equal(t, buckets, r.buckets)
 }
 
@@ -163,10 +166,14 @@ func TestHistogramDurationSamples(t *testing.T) {
 		h.RecordDuration(offset +
 			time.Duration(rand.Float64()*float64(10*time.Millisecond)))
 	}
+	offset = 60 * time.Millisecond
+	h.RecordDurationWithWeight(offset+
+		time.Duration(rand.Float64()*float64(10*time.Millisecond)), 2)
 
 	h.report(h.name, h.tags, r)
 
 	assert.Equal(t, 3, r.durationSamples[10*time.Millisecond])
 	assert.Equal(t, 5, r.durationSamples[60*time.Millisecond])
+	assert.Equal(t, 2, r.durationSamples[70*time.Millisecond])
 	assert.Equal(t, buckets, r.buckets)
 }
